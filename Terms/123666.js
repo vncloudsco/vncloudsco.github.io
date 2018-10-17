99d2f99291be7708c6a255df565d716d4e19b6c4
vncloudsco <41409442+vncloudsco@users.noreply.github.com>
2018-10-17 11:55:51 +0700

    Update 123666.js

--- Terms/123666.js
+++ Terms/123666.js
@@ -1,7 +1,7 @@
 noddityConfig = { // Global
 	name: 'Noddity',
 	noddityRoot: location.pathname + 'content/',
-	editLink: 'https://github.com/TehShrike/noddity/edit/master/content/',
+	editLink: 'https://github.com/vncloudsco/vncloudsco.github.io/tree/master/Terms/content',
 	logo: 'logo.svg',
 	errorPage: '404.md',
 	pathPrefix: '#!/',
